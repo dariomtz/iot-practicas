acac518a13f37505d7cface38ccbda249acbf0d9
Dario Martinez <dariom@lyft.com>
2023-09-13 12:30:41 -0600

    Terminada

--- practica2/README.md
+++ practica2/README.md
@@ -4,9 +4,26 @@ En esta práctica aprendemos:
 
 1. Prender y apagar el led interno del ESP32, escribir la temperatura interna del ESP32 en el monitor serial
 
-  [Código](./practica2-1/practica2-1.ino)
+    [Código](./practica2-1/practica2-1.ino)
+
+    [Video](https://photos.app.goo.gl/i5soALeWznu8G23Y7)
+
 
 2. Usar el ESP32 como servidor web
+
+    [Código](./practica2-2/practica2-2.ino)
+
+    ![Imagen1](./img/img1.png)
+
+    ![Imagen2](./img/img2.png)
+
 3. Opciones para alimentar el ESP32 cuando esta conectado a wifi
+    
+    Para este paso utilizamos el mismo código del paso anterior
+
+    [Video](https://photos.app.goo.gl/XUuWdxbz1z5Cz9MC8)
+
 4. Prender y apagar el led interno del ESP32 desde el servidor web
+
+
 5. Prender y apagar un led externo desde el servidor web ESP32
--- practica2/practica2-templates/practica2-templates.ino
+++ practica2/practica2-templates/practica2-templates.ino
@@ -1,64 +1,73 @@
 #include <json.hpp>
 #include <inja.hpp>
-#include <SPIFFS.h>
-#include <WiFi.h>
-WiFiServer server(80);
-WiFiClient client;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-uint8_t temprature_sens_read();
-#ifdef __cplusplus
-}
-#endif
+// #include <SPIFFS.h>
+// #include <WiFi.h>
+// WiFiServer server(80);
+// WiFiClient client;
 
-uint8_t temprature_sens_read();
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+// uint8_t temprature_sens_read();
+// #ifdef __cplusplus
+// }
+// #endif
+
+// uint8_t temprature_sens_read();
 
 void setup()
 {
   Serial.begin(9600);
-  WiFi.disconnect();
-  delay(3000);
-  Serial.println("Iniciando");
-  WiFi.begin("IoT","1t3s0IoT23");
-  while ((!(WiFi.status() == WL_CONNECTED))){
-    Serial.print("......");
-    delay(300);
-  }
-  Serial.println("Conexion establecida con el SSDI!");
-  Serial.println((WiFi.localIP()));
+  nlohmann::json data;
+  data["firstName"] = "John";
+  data["lastName"] = "Smith";
+ 
+  // std::string templateString = "hello {{firstName}} {{lastName}}.";
+   
+  // std::string result = inja::render(templateString, data);
+  // Serial.println(result.c_str());
+  // WiFi.disconnect();
+  // delay(3000);
+  // Serial.println("Iniciando");
+  // WiFi.begin("IoT","1t3s0IoT23");
+  // while ((!(WiFi.status() == WL_CONNECTED))){
+  //   Serial.print("......");
+  //   delay(300);
+  // }
+  // Serial.println("Conexion establecida con el SSDI!");
+  // Serial.println((WiFi.localIP()));
 
-  if (!SPIFFS.begin(true)) {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
+  // if (!SPIFFS.begin(true)) {
+  //   Serial.println("An Error has occurred while mounting SPIFFS");
+  //   return;
+  // }
 
-  server.begin();
+  // server.begin();
 }
 
 void loop()
 {
-  client = server.available();
-  if (!client) { return; }
-  while(!client.available()){ delay(1); }
+  // client = server.available();
+  // if (!client) { return; }
+  // while(!client.available()){ delay(1); }
   	
-  nlohmann::json data;
+  // // nlohmann::json data;
 
-  data["secondsActive"] = (millis()/1000);
-  data["temperature"] = ((temprature_sens_read() - 32 ) / 1.8);
+  // // data["secondsActive"] = (millis()/1000);
+  // // data["temperature"] = ((temprature_sens_read() - 32 ) / 1.8);
 
-  inja::Environment env;
+  // // inja::Environment env;
 
   	
-  inja::Template temp = env.parse_template("/spiffs/template.txt");
-  std::string result = env.render(temp, data);
+  // // inja::Template temp = env.parse_template("/spiffs/template.txt");
+  // // std::string result = env.render(temp, data);
 
-  client.flush();
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("");
-  client.println()
+  // client.flush();
+  // client.println("HTTP/1.1 200 OK");
+  // client.println("Content-Type: text/html");
+  // client.println("");
+  // //client.println(result);
 
-  delay(1);
+  // delay(1);
 }
