195b7fff996eea4b5ecd633f3914ae0bcd70905f
Dario Martinez <dariom@lyft.com>
2023-09-19 19:34:24 -0600

    Remove password

--- practica2/practica2-2/practica2-2.ino
+++ practica2/practica2-2/practica2-2.ino
@@ -1,7 +1,8 @@
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
-uint8_t temprature_sens_read();
+  uint8_t temprature_sens_read();
 #ifdef __cplusplus
 }
 #endif
@@ -18,8 +19,9 @@ void setup()
   WiFi.disconnect();
   delay(3000);
   Serial.println("Iniciando");
-  WiFi.begin("MEGACABLE-2.4G-82AB","WpW4TA3XBF");
-  while ((!(WiFi.status() == WL_CONNECTED))){
+  WiFi.begin("IoT", "1t3s0IoT23");
+  while ((!(WiFi.status() == WL_CONNECTED)))
+  {
     Serial.print("......");
     delay(300);
   }
@@ -30,8 +32,14 @@ void setup()
 void loop()
 {
   client = server.available();
-  if (!client) { return; }
-  while(!client.available()){ delay(1); }
+  if (!client)
+  {
+    return;
+  }
+  while (!client.available())
+  {
+    delay(1);
+  }
   client.flush();
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
@@ -39,33 +47,51 @@ void loop()
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
   client.println("<head>");
-  client.println("<title> ""MiESP32"" </title>");
+  client.println("<title> "
+                 "MiESP32"
+                 " </title>");
   client.println("</head>");
   client.println("<body>");
-  client.println("<h1 style=""color:#3366ff"">");
+  client.println("<h1 style="
+                 "color:#3366ff"
+                 ">");
   client.println("");
   client.println("</h1>");
-  client.println("<span style=""color:#33cc00;font-size:14px"">");
+  client.println("<span style="
+                 "color:#33cc00;font-size:14px"
+                 ">");
   client.println("Bienvenidos a mi pagina web");
   client.println("</span>");
   client.println("<br>");
-  client.println("<span style=""color:#cc6600;font-size:14px"">");
+  client.println("<span style="
+                 "color:#cc6600;font-size:14px"
+                 ">");
   client.println("Esta pagina web ha estado activa por");
   client.println("</span>");
-  client.println("<span style=""color:#009900;font-size:20px"">");
-  client.println((millis()/1000));
+  client.println("<span style="
+                 "color:#009900;font-size:20px"
+                 ">");
+  client.println((millis() / 1000));
   client.println("</span>");
-  client.println("<span style=""color:#cc6600;font-size:14px"">");
+  client.println("<span style="
+                 "color:#cc6600;font-size:14px"
+                 ">");
   client.println("segundos");
   client.println("</span>");
   client.println("<br>");
-  client.println("<span style=""color:#ffcc33;font-size:14px"">");
+  client.println("<span style="
+                 "color:#ffcc33;font-size:14px"
+                 ">");
   client.println("La temperatura interna del ES32 es de:");
   client.println("</span>");
-  client.println("<span style=""color:#000000;font-size:14px"">");
-  client.println(((temprature_sens_read() - 32 ) / 1.8));
+  client.println("<span style="
+                 "color:#000000;font-size:14px"
+                 ">");
+  client.println(((temprature_sens_read() - 32) / 1.8));
   client.println("</span>");
-  client.println("<span style=""color:#ffcc33;font-size:14px"">");
+  client.println("<span style="
+                 "color:#ffcc33;font-size:14px"
+                 ">");
   client.println("grados");
   client.println("</span>");
   client.println("</body>");
